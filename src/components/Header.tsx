d141861f7154a8aa51752a462a080d9e5043d7d9
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 02:00:52 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,10 @@ const Header: React.FC = () => {
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-center">
                     <img src="/images/logo.png" alt="Kryptium Logo" className="inline-block mx-auto mr-2" />
-                    <span className="text-2xl font-semibold text-gray-800 dark:text-gray-200">Kryptium</span>
+                    <span className="text-2xl font-semibold text-gray-800 dark:text-gray-200">Kryptium Finance</span>
                 </div>
                 <nav className="hidden md:flex flex-grow justify-center">
-                    <ul className="flex items-center space-x-8">
+                    <ul className="flex items-center space-x-6">
                         <li className="text-gray-800 dark:text-gray-200 font-semibold">Dashboard</li>
                         <li className="text-gray-800 dark:text-gray-200 font-semibold">My NFTs</li>
                         <li className="text-gray-800 dark:text-gray-200 font-semibold">Docs</li>
