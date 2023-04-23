3ec180e22478e2a6d7c9dd1ce6f9d411e0b38df3
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 01:29:56 -0500

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer: React.FC = () => {
+    return (
+        <footer className="...">
+            <a href="https://twitter.com/..." target="_blank" rel="noopener noreferrer">Twitter</a>
+            <a href="https://discord.com/..." target="_blank" rel="noopener noreferrer">Discord</a>
+            <a href="https://telegram.org/..." target="_blank" rel="noopener noreferrer">Telegram</a>
+        </footer>
+    );
+};
+
+export default Footer;
