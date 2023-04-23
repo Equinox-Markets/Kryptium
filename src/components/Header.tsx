8fdf53ee19dbd41a1e359e95400df9c822516869
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 01:41:11 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,24 @@ import ConnectWallet from './ConnectWallet';
 
 const Header: React.FC = () => {
     return (
-        <header className="...">
-            <div className="...">
-                <img src="/images/logo.png" alt="Kryptium Logo" />
+        <header className="bg-white dark:bg-gray-800 py-4 shadow-md">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-center">
+                    <img src="/images/logo.png" alt="Kryptium Logo" className="inline-block mx-auto" />
+                </div>
+                <nav className="hidden md:flex">
+                    <ul className="flex items-center space-x-4">
+                        <li className="text-gray-800 dark:text-gray-200 font-semibold">Dashboard</li>
+                        <li className="text-gray-800 dark:text-gray-200 font-semibold">My NFTs</li>
+                    </ul>
+                </nav>
+                <div className="absolute top-4 right-4">
+                    <ConnectWallet />
+                </div>
             </div>
-            <nav className="...">
-                <ul className="...">
-                    <li>Dashboard</li>
-                    <li>My NFTs</li>
-                </ul>
-            </nav>
-            <ConnectWallet />
         </header>
     );
 };
 
 export default Header;
+
