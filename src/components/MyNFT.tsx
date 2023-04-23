ac8f91bc6776aa75a6aca38b5bb0e63943d5f1a2
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 03:07:17 -0500

    Update MyNFT.tsx

--- src/components/MyNFT.tsx
+++ src/components/MyNFT.tsx
@@ -1,38 +1,44 @@
-// MyNFT.tsx
-const MyNFT: React.FC = () => {
-    return <div className="my-nfts"><div className="bg-white dark:bg-gray-700 shadow-md rounded-lg p-6 mt-6">
-      <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">My NFTs</h2>
-      <div className="flex justify-center">
-        {address ? (
-          userNFTs.length > 0 ? (
-            userNFTs.map((nft, index) => (
-              // Replace with the JSX structure you want to display for each NFT
-              <div key={index}>NFT {index + 1}: {nft}</div>
-            ))
-          ) : (
-            <p className="text-gray-500 dark:text-gray-400">
-              {noNFTMessage}
-              <a
-                href="https://example.com" // Replace with the actual website URL
-                target="_blank"
-                rel="noreferrer"
-                className="text-blue-500 hover:text-blue-600"
-              >
-                You do not own any NFTs. You can purchase them here
-              </a>
-            </p>
-          )
-        ) : (
-          <p className="text-gray-500 dark:text-gray-400">
-            Please connect your wallet to view your NFTs
-          </p>
+import React, { useState, useEffect } from 'react';
+
+const MyNFTs: React.FC = () => {
+  const [nfts, setNfts] = useState([]); // Replace with the appropriate data type for your NFTs
+
+  // Load NFTs from the specific collection by connecting to the user's wallet
+  const loadNFTs = async () => {
+    // Implement your logic to load NFTs from the connected wallet
+    // Then update the state with the loaded NFTs
+  };
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  return (
+    <section id="my-nfts" className="py-8">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-semibold mb-4">My NFTs</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {nfts.map((nft, index) => (
+            <div key={index} className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-6">
+              <img
+                src={nft.image} // Replace with the appropriate image property from your NFT data
+                alt={`NFT ${nft.name}`} // Replace with the appropriate name property from your NFT data
+                className="w-full h-64 object-cover object-center rounded-md"
+              />
+              <h3 className="text-xl font-semibold mt-4 text-gray-900 dark:text-gray-100">{nft.name}</h3>
+              <p className="text-gray-500 dark:text-gray-400">{nft.description}</p>
+            </div>
+          ))}
+        </div>
+        {nfts.length === 0 && (
+          <div className="mt-4 text-center text-gray-500 dark:text-gray-400">
+            <p>No NFTs found in your collection.</p>
+          </div>
         )}
-        </div>
-      {!address && (
-        <div className="flex justify-center mt-4">
-          <ConnectWallet onClick={handleConnectWallet} />
-        </div>
-      )}
-    </div></div>;
+      </div>
+    </section>
+  );
 };
-export default MyNFT;
+
+export default MyNFTs;
+
