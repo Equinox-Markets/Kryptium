b9e1be200eb49d886d12b473f974090ddeb8645a
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 03:43:49 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,28 @@ import Header from '@/components/Header';
 import Dashboard from '@/components/Dashboard';
 import MyNFT from '@/components/MyNFT';
 import Footer from '@/components/Footer';
-import ThemeSwitcher from '@/components/ThemeSwitcher';
+import ThemeProvider from '@/context/ThemeContext';
 
 const Home: FC = () => {
     return (
-        <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900">
-            <Header />
-            <div className="flex-grow">
-                <div className="max-w-6xl mx-auto sm:px-6 lg:px-8 py-8">
-                    <div className="grid grid-cols-1 gap-8">
-                        <Dashboard />
-                        <MyNFT />
+        <ThemeProvider>
+            <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900">
+                <Header />
+                <div className="flex-grow">
+                    <div className="max-w-6xl mx-auto sm:px-6 lg:px-8 py-8">
+                        <div className="grid grid-cols-1 gap-8">
+                            <Dashboard />
+                            <MyNFT />
+                        </div>
                     </div>
                 </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        </ThemeProvider>
     );
 };
 
 export default Home;
+
 
 
