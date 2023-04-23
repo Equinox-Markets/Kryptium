f609401e2c808bd46ad97ab06c95ea0d2cbb7d03
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 02:40:13 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,18 +9,18 @@ const Header: React.FC = () => {
                 <div className="text-center">
                     <img src="/images/logo.png" alt="Kryptium Logo" className="inline-block mx-auto mr-2" />
                 </div>
-                <nav className="hidden md:flex flex-grow justify-center">
-                 <ul className="flex justify-center items-center space-x-4">
-                    <li>
-                        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
-                            Dashboard
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://discord.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
-                            MyNFTs
-                        </a>
-                    </li>
+                <nav className="hidden md:flex">
+                 <ul className="flex items-center space-x-8">
+                        <li>
+                            <a href="#dashboard" className="text-gray-800 dark:text-gray-200 font-semibold">
+                                Dashboard
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#my-nfts" className="text-gray-800 dark:text-gray-200 font-semibold">
+                                My NFTs
+                            </a>
+                        </li>
                     <li>
                         <a href="https://telegram.org" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
                             Docs
