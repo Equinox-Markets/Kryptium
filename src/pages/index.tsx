1c7609582cec82de21339e835ccf03dae210b919
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 03:20:52 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,8 @@ import Footer from '@/components/Footer';
 import ThemeSwitcher from '@/components/ThemeSwitcher';
 
 const Home: FC = () => {
-  const handleMintNow = () => {
-    console.log('Mint now button clicked');
-    // Add your minting functionality here
-  };
-
-  return (
-        <div className="relative flex items-top justify-center min-h-screen bg-gray-900 dark:bg-gray-100 sm:items-center py-4 sm:pt-0">
+    return (
+        <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900">
             <Header />
             <div className="flex-grow">
                 <div className="max-w-6xl mx-auto sm:px-6 lg:px-8 py-8">
