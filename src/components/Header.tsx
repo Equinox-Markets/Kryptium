6f1dc364a616e7ac05263bd9e3aa76c274467502
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 01:54:35 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
                     <img src="/images/logo.png" alt="Kryptium Logo" className="inline-block mx-auto mr-2" />
                     <span className="text-2xl font-semibold text-gray-800 dark:text-gray-200">Kryptium</span>
                 </div>
-                <nav className="hidden md:flex">
+                <nav className="hidden md:flex flex-grow justify-center">
                     <ul className="flex items-center space-x-12">
                         <li className="text-gray-800 dark:text-gray-200 font-semibold">Dashboard</li>
                         <li className="text-gray-800 dark:text-gray-200 font-semibold">My NFTs</li>
@@ -26,6 +26,7 @@ const Header: React.FC = () => {
 };
 
 export default Header;
+
 
 
 
