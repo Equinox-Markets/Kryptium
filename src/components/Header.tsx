23b115e926a3f33b91b326ad31d722b1d25fcba2
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 02:45:32 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,10 @@ const Header: React.FC = () => {
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-center">
                     <img src="/images/logo.png" alt="Kryptium Logo" className="inline-block mx-auto mr-2" />
+                    <span className="text-2xl font-semibold text-gray-800 dark:text-gray-200">Kryptium</span>
                 </div>
                 <nav className="hidden md:flex">
-                 <ul className="flex items-center space-x-8">
+                    <ul className="flex items-center space-x-8">
                         <li>
                             <a href="#dashboard" className="text-gray-800 dark:text-gray-200 font-semibold">
                                 Dashboard
@@ -22,11 +23,11 @@ const Header: React.FC = () => {
                             </a>
                         </li>
                         <li>
-                            <a href="https://telegram.org" className="text-gray-800 dark:text-gray-200 font-semibold">
-                            Docs
+                            <a href="#docs" className="text-gray-800 dark:text-gray-200 font-semibold">
+                                Docs
                             </a>
                         </li>
-                 </ul>
+                    </ul>
                 </nav>
                 <div className="flex items-center space-x-4 absolute top-4 right-4">
                     <ThemeSwitcher />
@@ -38,6 +39,7 @@ const Header: React.FC = () => {
 };
 
 export default Header;
+
 
 
 
