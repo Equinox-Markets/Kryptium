ecb311f6acff8aa6fd54b1f5a424b126b51d446c
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 04:01:55 -0500

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -6,7 +6,13 @@ const Dashboard: React.FC = () => {
             <div className="container mx-auto">
                 <h2 className="text-3xl font-semibold mb-4">Welcome to Kryptium</h2>
                 <div className="flex items-center space-x-4">
-                    <img src="/path/to/your/image.jpg" alt="Sample Image" className="w-1/3" />
+                    <Image
+                        src="/images/logo.png"
+                        alt="Kryptium Logo"
+                        width={500}
+                        height={500}
+                        className="inline-block mx-auto mr-2"
+                    />
                     <div className="w-2/3">
                         <p className="text-left">
                             Kryptium is a decentralized financial platform that aims to create a fair, economically sustainable, and mutually beneficial ecosystem for all participants.
