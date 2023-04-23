4ede2b5b8ff459108ec6ba8762d2bf73e453ba50
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 04:17:06 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,46 +3,54 @@ import ConnectWallet from './ConnectWallet';
 import Image from 'next/image';
 
 const Header: React.FC = () => {
-    return (
-        <header className="bg-white dark:bg-gray-800 py-4 shadow-md">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="text-center">
-                    <Image
-                        src="/images/logo.png"
-                        alt="Kryptium Logo"
-                        width={50}
-                        height={50}
-                        className="inline-block mx-auto mr-2"
-                    />
-                </div>
-                <nav className="hidden md:flex">
-                    <ul className="flex items-center space-x-8">
-                        <li>
-                            <a href="#dashboard" className="text-gray-800 dark:text-gray-200 font-semibold">
-                                Dashboard
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#my-nfts" className="text-gray-800 dark:text-gray-200 font-semibold">
-                                My NFTs
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#docs" className="text-gray-800 dark:text-gray-200 font-semibold">
-                                Docs
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <div className="flex items-center space-x-4 absolute top-4 right-4">
-                    <ConnectWallet />
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className="bg-white dark:bg-gray-800 py-4 shadow-md relative">
+      <div className="container mx-auto">
+        <Image
+          src="/images/logo.png"
+          alt="Kryptium Logo"
+          width={50}
+          height={50}
+          className="absolute top-4 left-4"
+        />
+        <nav className="hidden md:block absolute top-4 left-1/4">
+          <ul className="flex items-center space-x-8">
+            <li>
+              <a
+                href="#dashboard"
+                className="text-gray-800 dark:text-gray-200 font-semibold"
+              >
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a
+                href="#my-nfts"
+                className="text-gray-800 dark:text-gray-200 font-semibold"
+              >
+                My NFTs
+              </a>
+            </li>
+            <li>
+              <a
+                href="#docs"
+                className="text-gray-800 dark:text-gray-200 font-semibold"
+              >
+                Docs
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="absolute top-4 right-4">
+          <ConnectWallet />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
+
 
 
 
