bead07948e521f1facb23ee30f242e8a69c1ef6e
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 02:33:10 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,11 +10,23 @@ const Header: React.FC = () => {
                     <img src="/images/logo.png" alt="Kryptium Logo" className="inline-block mx-auto mr-2" />
                 </div>
                 <nav className="hidden md:flex flex-grow justify-center">
-                    <ul className="flex items-center">
-                        <li className="text-gray-800 dark:text-gray-200 font-semibold">Dashboard</li>
-                        <li className="text-gray-800 dark:text-gray-200 font-semibold">My NFTs</li>
-                        <li className="text-gray-800 dark:text-gray-200 font-semibold">Docs</li>
-                    </ul>
+                 <ul className="flex justify-center items-center space-x-4">
+                    <li>
+                        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
+                            Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://discord.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
+                            MyNFTs
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://telegram.org" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
+                            Docs
+                        </a>
+                    </li>
+                 </ul>
                 </nav>
                 <div className="flex items-center space-x-4 absolute top-4 right-4">
                     <ThemeSwitcher />
