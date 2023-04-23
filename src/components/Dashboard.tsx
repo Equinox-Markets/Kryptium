4a2bd707e472971f8c91f824ada8f4d9c0d64f3c
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 01:29:20 -0500

    Create Dashboard.tsx

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+// Dashboard.tsx
+const Dashboard: React.FC = () => {
+    return <div className="...">Dashboard content here</div>;
+};
+
+export default Dashboard;
+
+// MyNFT.tsx
+const MyNFT: React.FC = () => {
+    return <div className="...">My NFT content here</div>;
+};
+export default MyNFT;
