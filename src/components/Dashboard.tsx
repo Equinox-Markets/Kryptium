2b97e07c5ed7c9933c3a86d2de5a79b4c98c98a3
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 04:10:48 -0500

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,20 +5,20 @@ const Dashboard: React.FC = () => {
     return (
         <section id="dashboard" className="py-8">
             <div className="container mx-auto">
-                <h2 className="text-3xl font-semibold mb-4">Welcome to Kryptium</h2>
                 <div className="flex items-center space-x-4">
+                    <Image
+                        src="/images/robot.png"
+                        alt="Kryptium Logo"
+                        width={500}
+                        height={500}
+                        className="inline-block mx-auto mr-2"
+                    />
                     <div className="w-2/3">
+                        <h2 className="text-3xl font-semibold mb-2">Welcome to Kryptium</h2>
                         <p className="text-left">
                             Kryptium is a decentralized financial platform that aims to create a fair, economically sustainable, and mutually beneficial ecosystem for all participants.
                         </p>
                     </div>
-                    <Image
-                        src="/images/logo.png"
-                        alt="Kryptium Logo"
-                        width={400}
-                        height={400}
-                        className="inline-block mx-auto mr-2"
-                    />
                 </div>
             </div>
         </section>
@@ -26,4 +26,5 @@ const Dashboard: React.FC = () => {
 };
 
 export default Dashboard;
+
 
