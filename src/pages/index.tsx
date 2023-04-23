51f13341aeed1bc3b441f7077d1084d20d23f6e7
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 03:53:20 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,20 +8,18 @@ import ThemeSwitcher from '@/components/ThemeSwitcher';
 
 const Home: FC = () => {
     return (
-        <ThemeSwitcher>
-            <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900">
-                <Header />
-                <div className="flex-grow">
-                    <div className="max-w-6xl mx-auto sm:px-6 lg:px-8 py-8">
-                        <div className="grid grid-cols-1 gap-8">
-                            <Dashboard />
-                            <MyNFT />
-                        </div>
+        <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900">
+            <Header />
+            <div className="flex-grow">
+                <div className="max-w-6xl mx-auto sm:px-6 lg:px-8 py-8">
+                    <div className="grid grid-cols-1 gap-8">
+                        <Dashboard />
+                        <MyNFT />
                     </div>
                 </div>
-                <Footer />
             </div>
-        </ThemeSwitcher>
+            <Footer />
+        </div>
     );
 };
 
