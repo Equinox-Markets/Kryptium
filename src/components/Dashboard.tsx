8f8aa2e897c7a0478f44dcf10d89398a84b0bade
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 03:22:23 -0500

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,7 +4,7 @@ const Dashboard: React.FC = () => {
     return (
         <section id="dashboard" className="py-8">
             <div className="container mx-auto">
-                <h2 className="text-3xl font-semibold mb-4">Dashboard</h2>
+                <h2 className="text-3xl font-semibold mb-4"></h2>
                 <p>This is the Dashboard section.</p>
             </div>
         </section>
