cceb4f2ffe72c817bd081956a6784b8692da417f
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 01:47:12 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,30 @@
+import React from 'react';
+
 const Footer: React.FC = () => {
     return (
-        <footer className="...">
-            <a href="https://twitter.com/..." target="_blank" rel="noopener noreferrer">Twitter</a>
-            <a href="https://discord.com/..." target="_blank" rel="noopener noreferrer">Discord</a>
-            <a href="https://telegram.org/..." target="_blank" rel="noopener noreferrer">Telegram</a>
+        <footer className="bg-white dark:bg-gray-800 py-4 mt-8">
+            <div className="container mx-auto text-center">
+                <ul className="flex justify-center items-center space-x-4">
+                    <li>
+                        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
+                            Twitter
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://discord.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
+                            Discord
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://telegram.org" target="_blank" rel="noopener noreferrer" className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
+                            Telegram
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </footer>
     );
 };
 
 export default Footer;
+
