c267a7fba06eef841641ae2816a43da8c20661d9
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-23 01:29:41 -0500

    Create ClaimRewardsButton.tsx

---
+++ src/components/ClaimRewardsButton.tsx
@@ -0,0 +1,13 @@
+const ClaimRewardsButton: React.FC = () => {
+    const handleClaimRewards = () => {
+        // Add the logic for claiming rewards here.
+    };
+
+    return (
+        <button className="..." onClick={handleClaimRewards}>
+            Claim Rewards
+        </button>
+    );
+};
+
+export default ClaimRewardsButton;
